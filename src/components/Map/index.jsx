39b2efd59ca78dfd0521f52a54f2192083d9c07d
Sean.Rennie <sean.rennie6@gmail.com>
2018-12-13 07:46:09 +0000

    used the container styles

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -5,7 +5,7 @@ import { withStyles } from '@material-ui/core/styles';
 
 import { Map, GoogleApiWrapper } from 'google-maps-react';
 
-const styles = () => ({
+const containerStyle = {
   container: {
     height: '100vh',
     width: '100vw',
@@ -13,13 +13,20 @@ const styles = () => ({
     justifyContent: 'center',
     alignItems: 'center'
   }
-});
+};
 
 const mapStyles = {
   margin: 'auto',
   width: '80%',
   height: '80%'
 };
+
+/**
+ *    GOAL:  To an interactive pseudo GIS map using the google maps API
+ *
+ *    1) return the LatLng from the map on click and use it to add a marker
+ *
+ */
 
 class MapContainer extends Component {
   state = {};
@@ -37,17 +44,16 @@ class MapContainer extends Component {
 
   render() {
     const { zoom, initialCenter } = this.state;
-    const { classes, google } = this.props;
+    const { google } = this.props;
     return (
-      <section className={classes.container}>
-        <Map
-          onClick={this.handleMapClick}
-          google={google}
-          zoom={zoom}
-          style={mapStyles}
-          initialCenter={initialCenter}
-        />
-      </section>
+      <Map
+        onClick={this.handleMapClick}
+        google={google}
+        zoom={zoom}
+        style={mapStyles}
+        containerStyle={containerStyle}
+        initialCenter={initialCenter}
+      />
     );
   }
 }
@@ -57,12 +63,11 @@ MapContainer.defaultProps = {
   initialCenter: {
     lat: -30.565528,
     lng: 29.232544
-  }
+  },
+  centerAroundCurrentLocation: true
 };
 
-export default withStyles(styles)(
-  GoogleApiWrapper({
-    apiKey: process.env.GOOGLE_MAPS_API_KEY,
-    LoadingContainer: CircularProgress
-  })(MapContainer)
-);
+export default GoogleApiWrapper({
+  apiKey: process.env.GOOGLE_MAPS_API_KEY,
+  LoadingContainer: CircularProgress
+})(MapContainer);
